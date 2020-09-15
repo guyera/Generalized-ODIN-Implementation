b86daa02ba0c6df401dc4474495b01070eff1f5a
mattolson93 <32203230+mattolson93@users.noreply.github.com>
2020-09-15 15:33:24 -0700

    simplifying cosine, adding EuclideanDeconf

--- code/deconfnet.py
+++ code/deconfnet.py
@@ -10,6 +10,57 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+def norm(x):
+    norm = torch.norm(x, p=2, dim=1)
+    x = x / (norm.expand(1, -1).t() + .0001)
+    return x
+
+class CosineDeconf(nn.Module):
+    def __init__(self, in_features, num_classes):
+        super(CosineDeconf, self).__init__()
+
+        self.h = nn.Linear(in_features, num_classes, bias= False)
+        self.init_weights()
+
+    def init_weights(self):
+        return None
+
+    def forward(self, x):
+        x = norm(x)
+        w = norm(self.h.weight)
+
+        ret = (torch.matmul(x,w.T))
+        return ret
+
+class EuclideanDeconf(nn.Module):
+    def __init__(self, in_features, num_classes):
+        super(EuclideanDeconf, self).__init__()
+
+        self.h = nn.Linear(in_features, num_classes, bias= False)
+        self.init_weights()
+
+    def init_weights(self):
+        return None
+
+    def forward(self, x):
+        ret = -((x -self.h.weight)**2)
+        return ret
+        
+class InnerDeconf(nn.Module):
+    def __init__(self, in_features, num_classes):
+        super(InnerDeconf, self).__init__()
+
+        self.h = nn.Linear(in_features, num_classes)
+        self.init_weights()
+
+    def init_weights(self):
+        return None
+
+    def forward(self, x):
+        return self.h(x)
+
+
 class DeconfNet(nn.Module):
     def __init__(self, underlying_model, in_features, num_classes, h):
         super(DeconfNet, self).__init__()
